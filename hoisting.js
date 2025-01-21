b3b7727cf4fabde4abcf43e58ea12bdffdd6a3e3
Shadan9113 <mdshadanzaheer@gmail.com>
2025-01-21 10:31:39 +0530

    hoisting example

--- hoisting.js
+++ hoisting.js
@@ -1,18 +1,18 @@
 // variable
 
-console.log(x)
-var x = 4; 
+console.log(x);
+var x = 4;
 // undefined
 
-console.log(y)
+console.log(y);
 let y = 5;
 // ReferenceError for both let and const
 
-
 a();
 const a = () => {
-    let x = 4;
-    console.log(x)
-}
+  let x = 4;
+  console.log(x);
+};
+// let and const are aloocoated in the memory script but the function and var are in the global scope thats     why let and const are goes in the temporal deadzone if we execute before initialization
 // it also gives refrence error
 // only function is getting hoisted
