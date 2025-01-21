b6062808c19a3718790dbf56b463a91381a1c49e
Shadan9113 <mdshadanzaheer@gmail.com>
2025-01-21 11:27:47 +0530

    closure with setTimeout

---
+++ closureSetTimeout.js
@@ -0,0 +1,11 @@
+function a(){
+    for(let i=1; i<=5; i++){
+        setTimeout(function(){
+            console.log(i);
+            
+        },i*1000)
+    }
+    console.log("javaScript")
+}
+a();
+
