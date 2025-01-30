78bc866b6a8b2d0f9b703ce22e0b2e1879840ddf
Shadan9113 <mdshadanzaheer@gmail.com>
2025-01-30 12:35:11 +0530

    closure example

--- closure.js
+++ closure.js
@@ -8,3 +8,15 @@ let z = a();
 z();
 
 // closure always return its chlid function or lexical 
+
+function outer(){
+    let count = 0;
+    return function inner(){
+        count++
+        return count;
+    }
+}
+let counter = outer();
+console.log(counter());
+console.log(counter());
+
