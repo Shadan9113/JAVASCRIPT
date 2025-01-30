4d27231056e0806bfe82cf231280a28d165ca02e
Shadan9113 <mdshadanzaheer@gmail.com>
2025-01-30 11:09:13 +0530

    event loop example

--- eventLoop.js
+++ eventLoop.js
@@ -5,3 +5,14 @@ setTimeout(() => console.log("setTimeout resolved"), 0);
 Promise.resolve().then(() => console.log("promise resolved"));
 
 console.log("end");
+
+// Priority Order of the Event Loop
+// Call Stack – Executes synchronous code first.
+// Microtask Queue (High Priority)
+// Promises (.then)
+// MutationObserver
+// Callback Queue (Low Priority)
+// setTimeout
+// setInterval
+// fetch
+// DOM events
